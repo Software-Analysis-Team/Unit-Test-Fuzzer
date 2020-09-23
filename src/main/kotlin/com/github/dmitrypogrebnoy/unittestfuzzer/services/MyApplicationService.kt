25d7eb8c14bd6469a3bca9decc11fa46800651bf
GitHub Action <action@github.com>
2020-09-23 10:59:13 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/dmitrypogrebnoy/unittestfuzzer/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dmitrypogrebnoy.unittestfuzzer.services
+
+import com.github.dmitrypogrebnoy.unittestfuzzer.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
