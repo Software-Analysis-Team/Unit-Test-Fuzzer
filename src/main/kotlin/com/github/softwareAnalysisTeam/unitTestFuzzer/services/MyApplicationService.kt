d369848dff34f7822edca12e57ffee81c30a78d6
Dmitry Pogrebnoy <pogrebnoy.inc@gmail.com>
2020-09-27 11:37:33 +0300

    Add randoop and update config

---
+++ src/main/kotlin/com/github/softwareAnalysisTeam/unitTestFuzzer/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.softwareAnalysisTeam.unitTestFuzzer.services
+
+import com.github.softwareAnalysisTeam.unitTestFuzzer.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
